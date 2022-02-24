ddc3ce3e736eb868d82607fb0edf6299dfe147ff
developjik <developjik@gmail.com>
2022-02-24 20:01:13 +0900

    feat #4: router 기본 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,26 @@
-import React from "react";
-import Container from "components/Container";
-import DetailPage from "pages/DetailPage";
-import LinkPage from "pages/LinkPage";
-import { ThemeProvider } from "styled-components";
-import GlobalStyle from "styles/GlobalStyle";
+import React from 'react'
+import Container from 'components/Container'
+import DetailPage from 'pages/DetailPage'
+import LinkPage from 'pages/LinkPage'
+import { ThemeProvider } from 'styled-components'
+import GlobalStyle from 'styles/GlobalStyle'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import { DataInterface } from 'common/interface'
 
 function App() {
   return (
     <>
       <GlobalStyle />
       <Container>
-        <LinkPage />
-        {/* <DetailPage /> */}
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<LinkPage />} />
+            <Route path="/:key" element={<DetailPage />} />
+          </Routes>
+        </BrowserRouter>
       </Container>
     </>
-  );
+  )
 }
 
-export default App;
+export default App
